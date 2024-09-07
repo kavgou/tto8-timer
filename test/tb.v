a4d60be02c812cabc769ad697d9eb714d0907534
kavgou <68931659+kavgou@users.noreply.github.com>
2024-09-07 12:29:42 -0400

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -16,15 +16,22 @@ module tb ();
   // Wire up the inputs and outputs:
   reg clk;
   reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
+  reg start;
+  reg  ui_in_count;
+  wire uo_out_timer_reached;
+
+
+   module tt_um_timer (
+    input  wire [3:0] ui_in_count,    // Dedicated inputs
+    output wire uo_out_timer_reached,   // Dedicated outputs
+    input  wire       start,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+      
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_timer tt_um_timer (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
@@ -32,12 +39,9 @@ module tb ();
       .VGND(1'b0),
 `endif
 
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
+      .ui_in_count  (ui_in_count),    // Dedicated inputs
+      .uo_out_timer_reached (uo_out_timer_reached),   // Dedicated outputs
+      .start    (start),      // enable - goes high when design is selected
       .clk    (clk),      // clock
       .rst_n  (rst_n)     // not reset
   );
